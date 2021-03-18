de6fe02e42726e5a6284c10364840cf8eeb4ab51
WahdelMW <WWoodhouse@spartaglobal.com>
2021-03-18 22:35:11 +0000

    Added logout button

--- src/main/java/com/sparta/wahdel/nbastatsspringproject/controller/TeamRestController.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.wahdel.nbastatsspringproject.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TeamRestController {
-}
